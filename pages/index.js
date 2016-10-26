cdfbe4321d0fad411bb1d3691452c00c1439daf8
Ellen Hong <e.hong@berkeley.edu>
2016-10-26 00:13:31 -0700

    added HTML, CSS of blog post view

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { config } from 'config';
+
+class Index extends React.Component {
+  render () {
+    return (
+      <div>
+        <Helmet
+          title={config.siteTitle}
+        />
+        <div className="body">
+          <div className="main-box">
+            <div className="main-text">
+              <p className="main-text-title">
+                Engaging the Berkeley Community in dialogue on Asia.
+              </p>
+              <p className="main-text-subtitle">
+                BSJAS is an Institute of East Asian Studies sponsored journal that strives to showcase the best undergraduate and graduate work in Asia-related fields.
+              </p>
+              <div className="main-button">
+                Submit your work
+              </div>
+              <div className="main-button">
+                Learn more
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Index;
