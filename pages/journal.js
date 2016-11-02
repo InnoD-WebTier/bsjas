4801a235175cba5202504357f4f307e3a5762ee4
Minterly <Minterly@users.noreply.github.com>
2016-11-01 19:36:32 -0700

    fix journal.js spacing

--- pages/journal.js
+++ pages/journal.js
@@ -22,72 +22,72 @@ class Journal extends React.Component {
       let tabs = [{title: 'Spring 2015'}, {title: 'Spring 2014'}, {title: 'Spring 2013'}, {title: 'Spring 2012'}, {title: 'Fall 2010'}];
       const HTMLify = function (item, i) {
         return (
-		  <div
-			key={i}
-			onClick={() => handleClick(i)}
-			className = "tab"
-		  >
-			{item.title}
-		  </div>
+	  <div
+		key={i}
+		onClick={() => handleClick(i)}
+		className = "tab"
+	  >
+		{item.title}
+	  </div>
         );
       }
       return (
-		<div className="tab-nav">
-		  {tabs.map(HTMLify)}
-		</div>
-	  );
+	<div className="tab-nav">
+	  {tabs.map(HTMLify)}
+	</div>
+      );
     }
 
     const tabContent = () => {
-	  const journals = info["journals"];
+      const journals = info["journals"];
       const activeTab = this.state.activeTab;
-	  const data = journals[activeTab];
-	  const journalContent = function (item) {
-		return (
-		  <div>
-			<div className="author">
-			  {item.author}
-			</div>
-			
-			<div className="title">
-			  {item.title}
-			</div>
-		  </div>
-		);
-	  }
+      const data = journals[activeTab];
+      const journalContent = function (item) {
+	return (
+	  <div>
+		<div className="author">
+		  {item.author}
+		</div>
+
+		<div className="title">
+		  {item.title}
+		</div>
+	  </div>
+	);
+      }
       return (
         <div className="tab-content">
-		  <div className="tab-title">
-		    {data.title}
-		  </div>
-		  
-		  	<img className="cover-img" src={data.cover} />
+	  <div className="tab-title">
+	    {data.title}
+	  </div>
 
-		  <div className="at-container">
-		  {data.content.map(journalContent)}
-		  </div>
-		</div>
+	  <img className="cover-img" src={data.cover} />
+
+	  <div className="at-container">
+	    {data.content.map(journalContent)}
+	  </div>
+	</div>
       );
     }
 	
-	const submitQA = () => {
-		const data = info["submit"];
-		
-		const submitContent = function (item) {
-		  return (
-			<div>
-			  <div className="question">
-				{item.question}
-			  </div>
-			  
-			  <div className="answer">
-				<div dangerouslySetInnerHTML={{__html: item.answer}} />
-			  </div>
-			</div>
-		  );
-		}
-		
-		return data.map(submitContent)
+    const submitQA = () => {
+      const data = info["submit"];
+
+      const submitContent = function (item) {
+  	return (
+	  <div>
+	    <div className="question">
+	      {item.question}
+	    </div>
+
+	    <div className="answer">
+	      <div dangerouslySetInnerHTML={{__html: item.answer}} />
+	    </div>
+	 </div>
+        );
+      }
+      
+    return data.map(submitContent)
     }
 	
 	
@@ -97,25 +97,25 @@ class Journal extends React.Component {
           title={config.siteTitle}
         />
 		
-		<div className="journals">
-		  <div className="content-header">
+	<div className="journals">
+	  <div className="content-header">
             <span>Journal / </span>
             <span className="subheader">Recent Publications</span>
           </div>
 		  
-		  {navTabs()}
-		  {tabContent()}
-		</div>
+	  {navTabs()}
+	  {tabContent()}
+	</div>
 		
         <div className="submit">
-		  <div className="content-header">
+	  <div className="content-header">
             <span>Journal / </span>
             <span className="subheader">Submit</span>
           </div>
 		  
-		  {submitQA()}
-		</div>
-	  </div>
+	  {submitQA()}
+	</div>
+      </div>
     );
   }
 }
