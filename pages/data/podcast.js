6de7b41a065e5a061e5f508122818fc393c5a4e0
Bohui Moon <bohuimoon@gmail.com>
2016-11-07 21:15:08 -0800

    podcast finished

---
+++ pages/data/podcast.js
@@ -0,0 +1,7 @@
+export default
+{
+	// Podcast description text
+	"description": "The BSJAS Podcast is a biweekly series featuring UC Berkeley faculty and students with an interest in Asia. The Podcast aims to produce lively discussion and debate as well as highlight exceptional work from different disciplines undertaken by professors, graduate students, and undergraduates. ",
+
+	"iframe_src": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/184440349&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true",
+}
