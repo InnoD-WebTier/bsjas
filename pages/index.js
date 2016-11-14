34bdafbd5b3e9b4d41e7c6473120b686a7fb81d2
Emilee Chen <emilee.ty.chen@gmail.com>
2016-11-14 15:07:21 -0800

    scss formatting

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,18 @@ class Index extends React.Component {
         <Helmet
           title={config.siteTitle}
         />
-        <div className="main-box">
-          <div className="main-text">
-            <p className="main-text-title">
+        <div className="Main">
+          <div className="Main__box">
+            <p className="Main__title">
               Engaging the Berkeley Community in dialogue on Asia.
             </p>
-            <p className="main-text-subtitle">
+            <p className="Main__subtitle">
               BSJAS is an Institute of East Asian Studies sponsored journal that strives to showcase the best undergraduate and graduate work in Asia-related fields.
             </p>
-            <div className="main-button">
+            <div className="Main__button">
               Submit your work
             </div>
-            <div className="main-button">
+            <div className="Main__button">
               Learn more
             </div>
           </div>
