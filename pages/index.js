f4ef5bdbffd4cfc75d804528e4c99e993fd66555
Emilee Chen <emilee.ty.chen@gmail.com>
2016-11-14 14:58:49 -0800

    landing page formatting done

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,18 @@ class Index extends React.Component {
         <Helmet
           title={config.siteTitle}
         />
-        <div id="main-box">
-          <div id="main-text">
-            <p class="main-text-title">
+        <div className="main-box">
+          <div className="main-text">
+            <p className="main-text-title">
               Engaging the Berkeley Community in dialogue on Asia.
             </p>
-            <p class="main-text-subtitle">
+            <p className="main-text-subtitle">
               BSJAS is an Institute of East Asian Studies sponsored journal that strives to showcase the best undergraduate and graduate work in Asia-related fields.
             </p>
-            <div class="main-button">
+            <div className="main-button">
               Submit your work
             </div>
-            <div class="main-button">
+            <div className="main-button">
               Learn more
             </div>
           </div>
